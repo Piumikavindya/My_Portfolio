d0089a9bacc12c0355183b12d571d69eb74b20e6
Piumi Kavindya <piyumikavindyappk@gmail.com>
2026-06-13 14:21:32 +0530

    update 5

--- my-portfolio/src/app/componets/HeroSection.jsx
+++ my-portfolio/src/app/componets/HeroSection.jsx
@@ -248,7 +248,7 @@ export default function HeroSection() {
           </div>
           <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[300px] lg:h-[300px] relative mt-20">
             <Image
-              src="/assets/myProf.jpeg"
+              src="/assets/my1.jpeg"
               alt="my"
               width={300}
               height={300}
